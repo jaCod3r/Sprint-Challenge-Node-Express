4c0b7c30078f11e9d7b49924d01837eda65f42d7
Juber Ahmed <a.juber@hotmail.com>
2018-04-06 14:41:22 -0400

    Completed action model route

---
+++ routes/actionModelRoute.js
@@ -0,0 +1,90 @@
+const express = require('express');
+
+const db = require('../data/helpers/actionModel.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db
+    .get()
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(error => {
+      res.status(404).json({ error: 'Some error' });
+    });
+}); //Tested works
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db
+    .get(id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(error => {
+      res.status(404).json({ error: 'Error' });
+    });
+}); //Tested works
+
+router.post('/', (req, res) => {
+  const action = req.body;
+  db
+    .insert(action)
+    .then(response => {
+      console.log('response', response);
+
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const update = req.body;
+
+  let action;
+
+  db
+    .update(id, update)
+    .then(count => {
+      if (count > 0) {
+        db.get(id).then(updatedAction => {
+          res.status(200).json(updatedAction);
+        });
+      } else {
+        res.status(404).json(error);
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  let action;
+
+  db
+    .get(id)
+    .then(response => {
+      user = { ...response[0] };
+      db
+        .remove(id)
+        .then(reponse => {
+          res.status(200).json(reponse);
+        })
+        .catch(error => {
+          res.status(500).json({ error: 'Error' });
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error => {
+        error: 'Error';
+      });
+    });
+}); //Tested works
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
-const morgan = require('morgan');
 const helmet = require('helmet');
-
+const morgan = require('morgan');
 const actionModelRoute = require('./routes/actionModelRoute');
-const projectModelRoute = require('./routes/projectModelRoute');
+// const projectModelRoute = require('./routes/projectModelRoute');
 
 const server = express();
 
-server.morgan('dev');
-server.use(helmet);
+server.use(morgan('dev'));
+server.use(helmet());
 server.use(express.json());
 
 server.use('/api/actions', actionModelRoute);
-server.use('/api/projects', projectModelRoute);
+// server.use('/api/projects', projectModelRoute);
 
 server.get('/', (req, res) => {
-    res.send({api: 'Running....'})
-})
+  res.send({ api: 'Running....' });
+});
 
 const port = 5000;
 server.listen(port, () => console.log('API running on port 5000'));
